0d6eefb52e85fa16408ecc0d9271c1f44d1e6964
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-02-08 20:20:37 -0600

    Update for 1.40.2_4113

--- include/Config.hpp
+++ include/Config.hpp
@@ -1,16 +1,11 @@
 #pragma once
 
-#include "main.hpp"
-
-#include <string>
-#include <unordered_map>
-
-//#include "beatsaber-hook/shared/config/config-utils.hpp"
 #include "config-utils/shared/config-utils.hpp"
+#include <string>
 
-DECLARE_CONFIG(Config,
+DECLARE_CONFIG(Config) {
     CONFIG_VALUE(IsStock, bool, "Mod Enabled", true);
     CONFIG_VALUE(MaxSpeed, float, "Max Scroll Speed", 600);
     CONFIG_VALUE(Accel, float, "Scroll Acceleration", 2.5);
     CONFIG_VALUE(IsLinear, bool, "Scrolling is Linear", false);
-)
+};
